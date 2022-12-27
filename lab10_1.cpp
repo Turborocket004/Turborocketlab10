aa98080490798635e88a552f113d3b16ae85f2ec
Turborocket004 <Turborocket004@gmail.com>
2022-12-27 16:28:35 +0700

    lab10_1done

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,11 +2,18 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+int main(){
 
+    double loan,rate,pay,inter,bal;
+    int x = 0;
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+    cin >> pay;
+                 
+       
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +27,31 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	while(loan > 0){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << x+1; 
+	cout << setw(13) << left << loan;
+	inter = loan*rate/100;
+	cout << setw(13) << left << inter;
+	cout << setw(13) << left << loan+inter;
+	if(loan+inter-pay < 0 ){
+	    pay = loan+inter;
+	}
+	cout << setw(13) << left << pay;
+	bal = loan+inter-pay;
+	cout << setw(13) << left << bal;
 	cout << "\n";	
+	
+	loan = bal;    
+	x++;    
+	
+	    
+	    
+	}
+	
+	
+	
+	
 	
 	return 0;
 }
