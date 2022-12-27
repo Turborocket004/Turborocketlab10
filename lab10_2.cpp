06e7d62c9094779b8462bbb9ec05289294fdec98
Turborocket004 <Turborocket004@gmail.com>
2022-12-27 16:32:41 +0700

    lab10_2done

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+
+int printO(int x,int y){
+if(x <= 0 or y <= 0){
+    cout << "Invalid input";
+}
+else{    
+	for(int z =0; z < x; z++){
+
+    for(int i =0; i < y; i++){
+        cout << 'O';
+        }
+        cout << "\n";
+		}
+    }
+    return 0;
+}
 
 int main(){
 	
